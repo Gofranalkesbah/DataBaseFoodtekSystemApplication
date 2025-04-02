ea3380656e2689ece4cc7f3b27ee297d24e9296a
Gofran Alkesbah <gofranalkesbah@gmail.com>
2025-04-03 01:41:30 +0300

    Db project by Gofran and Reem

---
+++ DB Script/Setup/SQLQuery insert.sql
@@ -0,0 +1,187 @@
+﻿
+
+INSERT INTO SuperAdmin  
+    (UserName, [password], Email, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate)  
+VALUES  
+    ('SuperAdmin1', 'Passw0rd@1', 'superadmin1@gmail.com', 'admin1', 'admin1', 0, GETDATE(), GETDATE()),  
+    ('SuperAdmin2', 'Passord@2', 'superadmin2@gmail.com', 'admin2', 'admin2', 0, GETDATE(), GETDATE());  
+
+INSERT INTO [Admin]
+    (FirstName, LastName, Email, UserName, PhoneNumber, [Password], 
+     ProfileImage, JoinDate, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, SuperAdmin_id)  
+VALUES  
+    ('Osama', 'Ahmad', 'Osama@gmail.com', 'Osama_Ahmad', '0791234567', 'Password123@',  
+     NULL, GETDATE(), 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1),
+
+    ('Eman', 'Mahmoud', 'Eman@gmail.com', 'Eman_Mahmoud', '0781234567', 'EmanPass123@',  
+     NULL, GETDATE(), 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2);  
+
+
+	 INSERT INTO [Role]  
+    (NameEN, NameAR, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Admin_id)  
+VALUES  
+    ('Manager', N'مدير', 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1),  
+    ('Driver', N'سائق', 'SuperAdmin2', 'SuperAdmin2', 0, GETDATE(), GETDATE(), 2);  
+
+
+	INSERT INTO Employee  
+    (FirstName, LastName, Email, UserName, PhoneNumber, [Password], ProfileImage,  
+     JoinDate, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Admin_id, Role_id)  
+VALUES  
+    ('Omar', 'Ali', 'Omar@gmail.com', 'Omar_Ali', '0792345678', 'Omar123@', 
+	NULL, GETDATE(), 'admin1', 'admin1', 0, GETDATE(), NULL, 1, 1),  
+
+    ('Yousef', 'Mahmoud', 'yousef.mahmoud@zoho.com', 'yousef.m', '0791122334', 'EmpP@ssw0rd',  
+     NULL, GETDATE(), 'admin2', 'admin2', 0, GETDATE(), NULL, 2, 1);  
+
+
+	 INSERT INTO Category  
+    (NameEN, NameAR, [Image], CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Admin_id)  
+VALUES  
+    ('Pizza', N'بيتزا', 'pizza.png', 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1),  
+    ('Seafood', N'مأكولات بحرية', 'seafood.png', 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2);  
+
+
+	INSERT INTO Client  
+    ([Image], FirstName, LastName, Email, PhoneNumber, JoinDate, [Status], CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate)  
+VALUES  
+    ('hala.jpg', 'Hala', 'Khaled', 'hala@gmail.com', '0773456789', GETDATE(), 'Active','admin1', 'admin1', 0, GETDATE(), GETDATE()),  
+
+    (NULL, 'Lina', 'Ahmad', 'lina.ahmad@hotmail.com', '0789876543', '2025-04-02 21:13:04.130', 'Not Activated Yet', 'Admin1', 'Admin2', 0, GETDATE(),GETDATE());  
+
+
+	INSERT INTO Driver  
+    (FirstName, LastName, Email, UserName, PhoneNumber, [Password], ProfileImage,  
+     JoinDate, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Admin_id)  
+VALUES  
+    ('Ahmed', 'Ali', 'ahmed.ali@gmail.com', 'ahmedali', '0783456789', 'Password789!', 'driver1.jpg',  
+     '2025-02-02', 'admin1', 'admin1', 0, GETDATE(), NULL, 1),  
+
+    ('Hassan', 'Salim', 'hassan.salim@outlook.com', 'hassan.s', '0776543219', 'Dr1v3rP@ss', NULL,  
+     '2025-04-02', 'admin2', 'admin2', 0, GETDATE(), NULL, 2);  
+
+
+	 INSERT INTO Deliveries  
+    (DeliveryStatues, PickUpLocation, DropLocation, CreatedBy, UpdatedBy,  
+     IsActive, UpdateDate, CreationDate, Client_id, Driver_id)  
+VALUES  
+    ('Pending', 'Amman, Jordan', 'Irbid, Jordan', 'Client1', 'Admin1',  
+     1, GETDATE(), GETDATE(), 1, 1),  
+
+    ('In-Transit', 'Irbid, Jordan', 'Aqaba, Jordan', 'Admin2', 'Admin2',  
+     0, GETDATE(), GETDATE(), 2, 2);  
+
+
+	 INSERT INTO Suggest  
+    (Title, Email, [Status], CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Client_id)  
+VALUES  
+    ('Add more vegetarian options', 'michael.jordan@gmail.com', 'Received', 'admin1', 'admin1',  
+     0, GETDATE(), GETDATE(), 1),  
+
+    ('Improve Delivery Time', 'client3@gmail.com', 'Received', 'admin2', 'admin2',  
+     0, GETDATE(), GETDATE(), 2);  
+
+
+	 INSERT INTO [Order]  
+    (TotalPrice, Rate, [Status], Actions, CreatedBy, UpdatedBy,  
+     IsActive, UpdateDate, CreationDate, Admin_id, Client_id, Driver_id, Deliveries_id)  
+VALUES  
+    (20.00, 5, 'Active', 'Order placed', 'admin1', 'admin1',  
+     0, GETDATE() , GETDATE(), 1, 1, 1, 1),  
+
+    (50.00, 4, 'Canceled', 'Order was canceled by the client', 'admin2', 'admin2',  
+     0, GETDATE() , GETDATE() , 2, 2, 2, 2);  
+
+	 INSERT INTO Item  
+    (NameEN, NameAR, DescriptionAR, DescriptionEN, Price, [Image], CreatedBy, UpdatedBy,  
+     IsActive, UpdateDate, CreationDate, Admin_id, Category_id)  
+VALUES  
+    ('Pizza', N'بيتزا', N'بيتزا إيطالية لذيذة', 'Delicious Italian pizza', 15.99, 'pizza.jpg',  
+     'admin', 'admin', 0, GETDATE() , GETDATE() , 1, 1),  
+
+    ('Grilled Shrimp', N'جمبري مشوي', N'جمبري مشوي متبل بالأعشاب والليمون', 'Grilled shrimp marinated with herbs and lemon', 12.99, 'grilled_shrimp.png',  
+     'admin2', 'admin2', 0, GETDATE() , GETDATE() , 2, 2);  
+
+	 INSERT INTO ItemOption  
+    (NameEN, NameAR, IsRequired, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Admin_id, Item_id)  
+VALUES  
+    ('Extra Cheese', N'جبن اضافي', 0, 'admin1', 'admin1', 0, GETDATE(), GETDATE() , 1, 1),  
+
+    ('Spicy Sauce', N'صلصة حارة', 0, 'admin2', 'admin2', 0, GETDATE(), GETDATE() , 2, 2);  
+
+
+	INSERT INTO [Discount&offers]  
+    (TitleEN, TitleAR, DescriptionEN, DescriptionAR, StartDate, EndDate, LimitAmount, Code, 
+	[Image], DiscountPercentage, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, 
+	Admin_id, Item_id, Order_id)  
+VALUES  
+    ('Pizza 20% Off', N'خصم 20% على البيتزا', 'Get 20% off on all pizza orders', N'احصل على خصم 20% على جميع طلبات البيتزا',
+	'2025-04-03 ', '2025-04-30', NULL, 'PIZZA123', NULL, 20, 'admin1', 'admin1', 0, GETDATE(),GETDATE(), 1, 1, 1),  
+
+    ('Grilled Shrimp Discount', N'خصم على الجمبري المشوي', 'Enjoy 15% off on grilled shrimp dishes.',
+	N'استمتع بخصم 15% على أطباق الجمبري المشوي', '2025-04-03', '2025-04-15', 
+	NULL, 'GRILL15', NULL, 15, 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2, 2, 2);
+
+
+	INSERT INTO Notifications  
+    (Title, NotificationType, IsRead, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate,
+	Admin_id, Employee_id, Client_id, Driver_id, [Discount&offers_id])  
+VALUES  
+    ('System Maintenance Alert', 'Issue', 0, 'Admin1', 'Admin1', 1, GETDATE(), GETDATE(), 1, 1, 1, 1, 1),  
+    ('Grilled Shrimp Dish Order Received', 'New Order', 0, 'Admin2', 'Admin2', 1, GETDATE(), GETDATE(), 2, 2, 2, 2, 2);
+
+
+	INSERT INTO Payment  
+    (Amount, PaymentType, PaymentStatues, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Client_id, Order_id)  
+VALUES  
+    (20.00, 'Credit Card', 'Completed', 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1, 1),  
+    (50.00, 'Credit Card', 'Completed', 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2, 2);
+
+
+	INSERT INTO Reviews  
+    (Rate, Comment, CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Client_id, Order_id, Driver_id, Item_id)  
+VALUES  
+    (5, 'Delicious pizza, will order again!', 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1, 1, 1, 1),  
+    (5, 'Excellent service and the seafood was amazing!', 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2, 2, 2, 2);
+
+
+	INSERT INTO Issue  
+    (Title, Email, [Status], CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Client_id, Notifications_id)  
+VALUES  
+    ('Wrong order received', 'michael.jordan@gmail.com', 'Open', 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1, 1),  
+    ('Food Delivery Delayed', 'client2@hotmail.com', 'Closed', 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2, 2);
+
+
+	INSERT INTO OrderHistory  
+    (TotalPrice, Rate, [Status], CreatedBy, UpdatedBy, IsActive, UpdateDate, CreationDate, Client_id, Order_id)  
+VALUES  
+    (20.00, 'Complete', 'Complete', 'admin1', 'admin1', 0, GETDATE(), GETDATE(), 1, 1),  
+    (50.00, 'Complete', 'Complete', 'admin2', 'admin2', 0, GETDATE(), GETDATE(), 2, 2);
+
+	INSERT INTO Client_Category (Client_id, Category_id)  
+VALUES  
+    (1, 1),  
+    (2, 2);
+
+	INSERT INTO Client_Item (Client_id, Item_id)  
+VALUES  
+    (1, 1),  
+    (2, 2);
+
+	INSERT INTO Client_Discount (Client_id, [Discount&offers_id])  
+VALUES  
+    (1, 1),  
+    (2, 2);
+
+	INSERT INTO Order_Item (Order_id, Item_id)  
+VALUES  
+    (1, 1),  
+    (2, 2);
+
+	INSERT INTO Category_Discount (Category_id, [Discount&offers_id])  
+VALUES  
+    (1, 1),  
+    (2, 2);
+
+
+
---
+++ DB Script/Tables/SQLQuery create.sql
@@ -0,0 +1,432 @@
+﻿create database [Foodtek System Application]
+
+create table SuperAdmin(
+Id int primary key identity(1,1),
+UserName varchar(15) unique not null check (UserName LIKE '[A-Za-z]%'),
+[password] varchar(50) not null check (
+    len([password]) >=8 and
+    [password] LIKE '%[a-z]%' and
+    [password] LIKE '%[A-Z]%' and 
+    [password] LIKE '%[0-9]%' and 
+    [password] LIKE '%[@$!%*?&]%'
+	),
+Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+),
+ CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate()
+)
+
+create table [Admin](
+ Id int primary key identity(1,1), 
+ FirstName varchar(50) not null check (FirstName LIKE '[A-Za-z]%' AND FirstName NOT LIKE '%[^A-Za-z ]%'), 
+ LastName varchar(50) not null check (LastName LIKE '[A-Za-z]%' AND LastName NOT LIKE '%[^A-Za-z ]%'), 
+ Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+), 
+ UserName varchar(50) not null, 
+ PhoneNumber varchar(10) not null unique check(
+               PhoneNumber like '07[789]_______' and
+               LEN(PhoneNumber) = 10),
+ [Password] varchar(50) not null check (
+    len([password]) >=8 and
+    [password] LIKE '%[a-z]%' and
+    [password] LIKE '%[A-Z]%' and 
+    [password] LIKE '%[0-9]%' and 
+    [password] LIKE '%[@$!%*?&]%'
+	), 
+ ProfileImage varchar(max), 
+ JoinDate datetime ,
+ CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ SuperAdmin_id int ,
+ foreign key(SuperAdmin_id) references SuperAdmin(id),
+)
+
+create table [Role](
+Id int primary key identity(1,1),
+NameEN varchar(50) not null unique check(NameEN LIKE '[A-Za-z ]%'),
+NameAR nvarchar(50) NOT NULL UNIQUE check(NameAR LIKE '%[^أ-ي ]%'),
+ CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+)
+
+create table Employee(
+ Id int primary key identity(1,1), 
+ FirstName varchar(50) not null check (FirstName LIKE '[A-Za-z]%' AND FirstName NOT LIKE '%[^A-Za-z ]%'), 
+ LastName varchar(50) not null check (LastName LIKE '[A-Za-z]%' AND LastName NOT LIKE '%[^A-Za-z ]%'), 
+ Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+), 
+ UserName varchar(50) not null, 
+ PhoneNumber varchar(10) not null unique check(
+               PhoneNumber like '07[789]_______' and
+               LEN(PhoneNumber) = 10),
+ [Password] varchar(50) not null check (
+    len([password]) >=8 and
+    [password] LIKE '%[a-z]%' and
+    [password] LIKE '%[A-Z]%' and 
+    [password] LIKE '%[0-9]%' and 
+    [password] LIKE '%[@$!%*?&]%'
+	), 
+ ProfileImage varchar(max), 
+ JoinDate datetime ,
+ CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime null ,
+ Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+ Role_id int ,
+ foreign key(Role_id) references [Role](id),
+)
+
+create table Client(
+id int primary key identity(1,1), 
+[Image] varchar(max),
+FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+), 
+
+PhoneNumber varchar(10) not null unique check(
+               PhoneNumber like '07[789]_______' and
+               LEN(PhoneNumber) = 10),
+JoinDate datetime ,
+[Status] VARCHAR(20) NOT NULL  check (Status IN ('Active', 'Not Activated Yet', 'Forbidden', 'Blocked')),
+ CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate()
+)
+
+create table [Order](
+Id int primary key identity(1,1), 
+TotalPrice decimal(10, 2) not null, 
+Rate int check(Rate between 1 AND 5),
+[Status] varchar(20) NOT NULL CHECK (
+        [Status] IN ('Active', 'On Shipment', 'Complete', 'Canceled')),
+Actions varchar(50) NOT NULL,
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+  Driver_id int ,
+  foreign key(Driver_id) references Driver(id),
+  Deliveries_id int,
+  foreign key(Deliveries_id) references Deliveries(id)
+)
+
+create table Issue (
+Id int primary key identity(1,1),
+Title varchar(255) not null , 
+Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+),
+[Status] varchar(30) not null check (
+        [Status] IN ('Open', 'Pending', 'Waiting Customer Response', 'Closed')
+    ),
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Notifications_id int ,
+ foreign key(Notifications_id) references Notifications(id),
+) 
+
+create table Suggest(
+Id int primary key identity(1,1),
+Title varchar(255) not null , 
+Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+), 
+[Status] varchar(30) not null check (
+        [Status] IN ('Received', 'Approved', 'Rejected')
+    ),
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+)
+
+create table Deliveries(
+Id int primary key identity(1,1),
+DeliveryStatues varchar(50) not null check (
+        DeliveryStatues in ('Pending', 'In-Transit', 'Delivered', 'Canceled')),
+PickUpLocation varchar(255) not null,
+DropLocation varchar(255) not null,
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit DEFAULT 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Driver_id int ,
+ foreign key(Driver_id) references Driver(id),
+)
+
+create table Category(
+Id int primary key identity(1,1), 
+NameEN varchar(50) not null unique check(NameEN LIKE '[A-Za-z ]%'),
+NameAR nvarchar(50) not null unique check(NameAR LIKE '%[^أ-ي ]%'),
+[Image] varchar(255) not null check([Image] LIKE '%.png' OR [Image] LIKE '%.jpg' OR [Image] LIKE '%.webp'),
+CreatedBy varchar(50) not null,
+UpdatedBy varchar(50) not null,
+IsActive Bit DEFAULT 0,
+UpdateDate datetime default GetDate(),
+CreationDate datetime default GetDate(),
+Admin_id int ,
+foreign key(Admin_id) references [Admin](id),
+)
+
+create table [Discount&offers](
+Id int primary key identity(1,1),
+TitleEN varchar(50) not null unique check(TitleEN LIKE '%[A-Za-z ]%'),
+TitleAR nvarchar(50) not null unique check(TitleAR LIKE '%[^أ-ي ]%'),
+DescriptionEN varchar(255) check (DescriptionEN LIKE '%[A-Za-z ]%'),
+DescriptionAR nvarchar(255) check (DescriptionAR LIKE '%[^أ-ي ]%'),
+StartDate datetime not null check (StartDate >= GETDATE()), 
+EndDate datetime not null check ( EndDate >= GETDATE()),
+LimitAmount float null check (LimitAmount > 0),
+Code varchar(50) check (Code NOT LIKE '%[^A-Za-z0-9]%'), 
+[Image] varchar(max) null,
+DiscountPercentage int null check (DiscountPercentage BETWEEN 1 AND 50),
+CreatedBy varchar(50) not null,
+UpdatedBy varchar(50) not null,
+IsActive Bit DEFAULT 0,
+UpdateDate datetime default GetDate(),
+CreationDate datetime default GetDate(),
+Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+ Item_id int, 
+ FOREIGN KEY(Item_id) REFERENCES Item(id) ,
+ Order_id int, 
+ FOREIGN KEY(Order_id) REFERENCES [Order](id)
+);
+
+create table Item(
+Id int primary key identity(1,1),
+NameEN varchar(50) not null unique check(NameEN LIKE '%[A-Za-z ]%'),
+NameAR nvarchar(50) not null unique check(NameAR LIKE '%[^أ-ي ]%'),
+DescriptionAR nvarchar(255) check (DescriptionAR LIKE '%[^أ-ي ]%'),
+DescriptionEN varchar(255) check (DescriptionEN LIKE '%[A-Za-z ]%'), 
+Price float check (Price > 0), 
+[Image] varchar(255) not null check([Image] LIKE '%.png' OR [Image] LIKE '%.jpg' OR [Image] LIKE '%.webp'),
+CreatedBy varchar(50) not null,
+UpdatedBy varchar(50) not null,
+IsActive Bit DEFAULT 0,
+UpdateDate datetime default GetDate(),
+CreationDate datetime default GetDate(),
+Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+   Category_id int ,
+ foreign key(Category_id) references Category(id),
+)
+
+create table ItemOption (
+Id int primary key identity(1,1),
+NameEN varchar(50) not null unique check(NameEN LIKE '[A-Za-z ]%'),
+NameAR nvarchar(50) not null unique check(NameAR LIKE '%[^أ-ي ]%'),
+IsRequired bit not null default 0,
+CreatedBy varchar(50) not null,
+UpdatedBy varchar(50) not null,
+IsActive Bit DEFAULT 0,
+UpdateDate datetime default GetDate(),
+CreationDate datetime default GetDate(),
+Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+  Item_id int ,
+ foreign key(Item_id) references Item(id),
+)
+
+create table Notifications(
+Id int primary key identity(1,1),
+Title varchar(50) not null  ,
+NotificationType varchar(100) not null check (
+                   NotificationType in ('Issue' , 'Support' , 'New Order' , 'New System Action')),
+IsRead bit not null default 0,
+CreatedBy varchar(50) not null,
+UpdatedBy varchar(50) not null,
+IsActive Bit DEFAULT 0,
+UpdateDate datetime default GetDate(),
+CreationDate datetime default GetDate(),
+Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+ Employee_id int ,
+ foreign key(Employee_id) references Employee(id),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+  Driver_id int ,
+ foreign key(Driver_id) references Driver(id),
+  [Discount&offers_id] int ,
+ foreign key([Discount&offers_id]) references [Discount&offers](id),
+)
+
+create table Driver(
+ Id int primary key identity(1,1), 
+ FirstName varchar(50) not null check (FirstName LIKE '[A-Za-z]%' AND FirstName NOT LIKE '%[^A-Za-z ]%'), 
+ LastName varchar(50) not null check (LastName LIKE '[A-Za-z]%' AND LastName NOT LIKE '%[^A-Za-z ]%'), 
+ Email varchar(50) unique check (
+           Email LIKE '_%@gmail.com' 
+        OR Email LIKE '_%@hotmail.com' 
+        OR Email LIKE '_%@outlook.com' 
+        OR Email LIKE '_%@zoho.com'
+), 
+ UserName varchar(50) not null, 
+ PhoneNumber varchar(10) not null unique check(
+               PhoneNumber like '07[789]_______' and
+               LEN(PhoneNumber) = 10),
+ [Password] varchar(50) not null check (
+    len([password]) >=8 and
+    [password] LIKE '%[a-z]%' and
+    [password] LIKE '%[A-Z]%' and 
+    [password] LIKE '%[0-9]%' and 
+    [password] LIKE '%[@$!%*?&]%'
+	), 
+ ProfileImage varchar(max), 
+ JoinDate datetime ,
+ CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit default 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime null ,
+ Admin_id int ,
+ foreign key(Admin_id) references [Admin](id),
+)
+
+create table Payment(
+Id int primary key identity(1,1),
+Amount decimal(18, 2) check (Amount > 0),
+PaymentType varchar(50) not null check (
+        PaymentType in ('Credit Card', 'Debit Card')) ,
+PaymentStatues varchar(50) not null check (
+        PaymentStatues in ('Pending', 'Completed', 'Failed')),
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit default 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Order_id int ,
+ foreign key(Order_id) references [Order](id),
+)
+
+create table Reviews(
+Id int primary key identity(1,1),
+Rate int check(Rate between 1 AND 5), 
+Comment varchar(250) null ,
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit default 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Order_id int ,
+ foreign key(Order_id) references [Order](id),
+  Driver_id int ,
+ foreign key(Driver_id) references Driver(id),
+  Item_id int ,
+ foreign key(Item_id) references Item(id),
+)
+
+create table OrderHistory(
+Id int primary key identity(1,1),
+TotalPrice decimal(10, 2) not null, 
+Rate varchar(50) not null check (Rate IN ('With Shipment', 'Canceled', 'Complete')),
+[Status] varchar(50) not null check ([Status] IN ('Active', 'On Shipment', 'Complete', 'Canceled')),
+CreatedBy varchar(50) not null,
+ UpdatedBy varchar(50) not null,
+ IsActive Bit default 0,
+ UpdateDate datetime default GetDate(),
+ CreationDate datetime default GetDate(),
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Order_id int ,
+ foreign key(Order_id) references [Order](id),
+)
+
+create table Client_Category(
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Category_id int ,
+ foreign key(Category_id) references Category(id),
+ PRIMARY KEY (Client_id, Category_id),
+)
+
+create table Client_Item(
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ Item_id int ,
+ foreign key(Item_id) references Item(id),
+ PRIMARY KEY (Client_id, Item_id),
+)
+
+create table Client_Discount(
+ Client_id int ,
+ foreign key(Client_id) references Client(id),
+ [Discount&offers_id] int ,
+ foreign key([Discount&offers_id]) references [Discount&offers](id),
+  PRIMARY KEY (Client_id, [Discount&offers_id]),
+)
+
+create table Order_Item(
+ Order_id int ,
+ foreign key(Order_id) references [Order](id),
+ Item_id int ,
+ foreign key(Item_id) references Item(id),
+  PRIMARY KEY (Order_id, Item_id ),
+)
+
+create table Category_Discount(
+ Category_id int ,
+ foreign key(Category_id) references Category(id),
+ [Discount&offers_id] int ,
+ foreign key([Discount&offers_id]) references [Discount&offers](id),
+   PRIMARY KEY ( Category_id, [Discount&offers_id] ),
+)
